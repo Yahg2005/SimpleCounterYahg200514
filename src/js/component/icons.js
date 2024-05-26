3534a03d86520007ce56d9a3876fcfa82b1336c2
Yahg2005 <161893815+Yahg2005@users.noreply.github.com>
2024-05-26 17:53:31 +0000

    First Commit

---
+++ src/js/component/icons.js
@@ -0,0 +1,7 @@
+import fontawesome from '@fortawesome/fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+
+fontawesome.config = {
+    autoReplaceSvg: 'nest'
+};
+fontawesome.library.add(faClock);
--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,59 @@
-//import react into the bundle
-import React from "react";
-import ReactDOM from "react-dom";
+// Importar React en el bundle
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Mover el import de Bootstrap aquí
+import '../styles/index.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-// include your styles into the webpack bundle
-import "../styles/index.css";
+library.add(faClock);
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props) {
+   
+    return (
+        <div className="container">
+        <div className="bigCounter">
+            <div className="calendary">
+            <FontAwesomeIcon icon={faClock} className="icon-style"/>
+            </div>
+            <div className="six">{props.digitSix% 10}</div>
+            <div className="five">{props.digitFive% 10}</div>
+            <div className="four">{props.digitFour% 10}</div>
+            <div className="three">{props.digitThree% 10}</div>
+            <div className="two">{props.digitTwo% 10}</div>
+            <div className="one">{props.digitOne % 10}</div>
+        </div>
+        </div>
+    );
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+    
+
+SimpleCounter.propTypes = {
+    // alertTime: PropTypes.number,
+    digitSix: PropTypes.number,
+    digitFive: PropTypes.number,
+    digitFour: PropTypes.number,
+    digitThree: PropTypes.number,
+    digitTwo: PropTypes.number,
+    digitOne: PropTypes.number,
+};
+
+let counter = 0;
+setInterval(function() {
+    const six = Math.floor(counter / 100000);
+    const five = Math.floor(counter / 10000);
+    const four = Math.floor(counter / 1000);
+    const three = Math.floor(counter / 100);
+    const two = Math.floor(counter / 10);
+    const one = Math.floor(counter / 1);
+    counter++;
+
+    // Renderizar tu aplicación de React
+    ReactDOM.render(
+        <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} digitFive={five} digitSix={six}/>,
+        document.querySelector("#app")
+    );
+},  1000);
